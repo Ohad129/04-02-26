497a9d641306ae4494ccdd9b2a1bc575a878408b
Ohad <2000ohad@gmail.com>
2026-02-05 21:26:42 +0200

    first commit

--- HW4.py
+++ HW4.py
@@ -10,17 +10,21 @@ while True:
     if last_name.isupper() and last_name.isalpha():
         break
     last_name = input('enter last name in uppercase: ')
+
 first_name = input('enter first name: ')
 while True:
     if first_name.islower() and first_name.isalpha():
         break
     first_name = input('enter first name in lowercase: ')
+
 country = input('enter country: ')
 while True:
     if len(country) <= 3 and country.isalpha() and country.isupper():
         break
     country = input('enter no more than 3 upper cased letters: ')
+
 city_address = input('enter city address: ')
+
 zipcode = input('enter zipcode: ')
 while True:
     if zipcode.isdigit() and len(zipcode) >= 4:
